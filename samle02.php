cdeccf2b0dcaac390fc9db0872cd1027b3b834f1
khadka2 <khadka2@gmail.com>
2015-05-13 04:39:30 +0000

    0513 practice

---
+++ samle02.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php print('PHP study!'); 
+    ?></p>
+    <a href="<?php print('http://h20-com');?>"></a>nerpal jirihzaszsvasas  bhv  good</a>
+    
+</body>
+</html>
